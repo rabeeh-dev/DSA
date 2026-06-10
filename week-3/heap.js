3cf7e21df5c45e2e8c30817bef628140fb824cda
Rabeeh <rbh.rabeeh00@gmail.com>
2026-06-10 12:39:36 +0530

    added heap questions

--- week-3/heap.js
+++ week-3/heap.js
@@ -1,96 +1,126 @@
-class MaxHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  insert(value) {
-    this.heap.push(value);
-    this.bubbleUp();
-  }
-
-  bubbleUp() {
-    let i = this.heap.length - 1;
-
-    while (i > 0) {
-      let parent = Math.floor((i - 1) / 2);
-
-      if (this.heap[i] > this.heap[parent]) {
-        let temp = this.heap[i];
-        this.heap[i] = this.heap[parent];
-        this.heap[parent] = temp;
-
-        i = parent;
-      } else {
-        break;
-      }
+class heap{
+    constructor(){
+        this.heap = []
     }
-  }
-
-  remove() {
-    // empty heap
-    if (this.heap.length === 0) {
-      return null;
+    
+    insert(data){
+        this.heap.push(data)
+        this.bubbleup()
     }
-
-    // only one element
-    if (this.heap.length === 1) {
-      return this.heap.pop();
+    
+    bubbleup(){
+        let i = this.heap.length-1
+        while(i > 0){
+            let parent = Math.floor((i-1)/2)
+            if(this.heap[i]>this.heap[parent]){
+                let temp = this.heap[i]
+                this.heap[i] = this.heap[parent]
+                this.heap[parent] = temp
+                
+                i = parent
+            }else{
+                break
+            }
+        }
     }
-
-    let max = this.heap[0];
-
-    // move last element to root
-    this.heap[0] = this.heap.pop();
-
-    // restore heap
-    this.bubbleDown();
-
-    return max;
-  }
-
-  bubbleDown() {
-    let i = 0;
-    while (true) {
-      let left = 2 * i + 1;
-      let right = 2 * i + 2;
-
-      let largest = i;
-
-      if (left < this.heap.length && this.heap[left] > this.heap[largest]) {
-        largest = left;
-      }
-
-      if (right < this.heap.length && this.heap[right] > this.heap[largest]) {
-        largest = right;
-      }
-
-      if (largest === i) {
-        break;
-      }
-
-      let temp = this.heap[i];
-      this.heap[i] = this.heap[largest];
-      this.heap[largest] = temp;
-
-      i = largest;
+    
+    remove(){
+        if(this.heap.length === 0) return null
+        if(this.heap.length === 1) return this.heap.pop()
+        let root = this.heap[0]
+        this.heap[0] = this.heap.pop()
+        this.bubbledown()
+        return root
     }
-  }
-
-  display() {
-    console.log(this.heap);
-  }
+    
+    bubbledown(length = this.heap.length){
+        let i = 0
+        
+        while(true){
+            let left = 2*i+1
+            let right = 2*i+2
+            let largest = i
+            
+            if(left < length && this.heap[left] > this.heap[largest]){
+                largest = left 
+            }
+            
+            if(right < length && this.heap[right] > this.heap[largest]){
+                largest = right
+            }
+            
+            if(largest !== i){
+                let temp = this.heap[i]
+                this.heap[i] = this.heap[largest]
+                this.heap[largest] = temp 
+                
+                i = largest
+            }else{
+                break
+            }
+        }
+    }
+    
+    heapSort(){
+        let length = this.heap.length
+        while(length > 1){
+            [this.heap[0],this.heap[length-1]] = 
+            [this.heap[length-1],this.heap[0]]
+            
+            length--
+            this.bubbledown(length)
+        }
+        return this.heap
+    }
+    
+    
+    display(){
+        console.log(this.heap)
+    }
 }
 
-const heap = new MaxHeap();
-heap.insert(100);
-heap.insert(70);
-heap.insert(95);
-heap.insert(20);
-heap.insert(50);
-heap.insert(80);
-heap.insert(90);
 
-heap.display();
-console.log("Removed :", heap.remove());
+const hp = new heap();
 
-heap.display();
+console.log("--- 1. INSERTING ELEMENTS ---");
+console.log("Inserting: 10, 20, 15, 30, 60");
+hp.insert(10);
+hp.insert(20);
+hp.insert(15);
+hp.insert(30);
+hp.insert(60);
+
+// Should output: [ 60, 30, 15, 10, 20 ]
+console.log("Heap after insertions:");
+hp.display(); 
+console.log("\n");
+
+
+console.log("--- 2. REMOVING MAX ELEMENT ---");
+const extractedMax = hp.remove();
+console.log(`Removed max element: ${extractedMax}`);
+
+// Should output: [ 30, 20, 15, 10 ]
+console.log("Heap after removal:");
+hp.display(); 
+console.log("\n");
+
+
+console.log("--- 3. HEAP SORT ---");
+console.log("Executing in-place heap sort...");
+const sortedData = hp.heapSort();
+
+// Should output: [ 10, 15, 20, 30 ]
+console.log("Heap sorted:", sortedData);
+console.log("==========================================");
+hp.display()
+hp.remove()
+hp.display()
+hp.heapSort()
+hp.display()
+
+
+
+
+
+
